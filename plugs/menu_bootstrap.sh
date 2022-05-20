2c6c3a8397b63e4d0ea83ecfd84f694ae9356a93
Morten Jakobsen <morten@jakeobsen.com>
2022-05-20 17:24:36 +0000

    Automatic bootstrap select based on product name

--- plugs/menu_bootstrap.sh
+++ plugs/menu_bootstrap.sh
@@ -1,6 +1,10 @@
 menu=""
 d='dialog --clear --stdout --title Unlab.dev'
-menu=$(dialog --clear --stdout --title "Unlab.dev" --menu "" 0 0 1 \
+
+grep -qi "Latitude E7270" /sys/class/dmi/id/product_name && menu="e7270"
+
+[ "$menu" == "" ] && menu=$(dialog --clear --stdout --title "Unlab.dev" --menu "" 0 0 1 \
         "e7270" "Bootstrap E7270" \
 )
+
 [ "$menu" != "" ] && curl -s https://install.unlab.dev/loader/$menu.sh | bash
