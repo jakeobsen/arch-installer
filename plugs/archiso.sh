1d737a44103e7da11ec2870ebf6529316f3d54b4
Morten Jakobsen <morten@jakeobsen.com>
2022-06-09 13:48:12 +0000

    Rename archlive folder

--- plugs/archiso.sh
+++ plugs/archiso.sh
@@ -3,7 +3,7 @@ source /tmp/env; [ "$envSourced" != "YES" ] && exit 255
 if [ "$iWantArchIso" == "yes" ]; then
 
 # Cleanup
-rm -rf /mnt/home/$newUsername/jakeobsen-archlive
+rm -rf /mnt/home/$newUsername/unlab-archlive
 
 # Allow running mkarchiso without password if in wheel
 cat>/mnt/etc/sudoers.d/mkarchiso<<EOF
@@ -14,25 +14,25 @@ EOF
 arch-chroot /mnt pacman -Sy --noconfirm git archiso git
 
 # Setup environment
-mkdir -p /mnt/home/$newUsername/jakeobsen-archlive/temp
-cd /mnt/home/$newUsername/jakeobsen-archlive
+mkdir -p /mnt/home/$newUsername/unlab-archlive/temp
+cd /mnt/home/$newUsername/unlab-archlive
 git clone https://github.com/jakeobsen/arch-archiso
-cp -r /tmp/env /mnt/home/$newUsername/jakeobsen-archlive/arch-archiso/airootfs/etc/bootstrap
+cp -r /tmp/env /mnt/home/$newUsername/unlab-archlive/arch-archiso/airootfs/etc/bootstrap
 
 # Reset all perms
 arch-chroot /mnt chown $newUsername:$newUsername -R /home/$newUsername
 
 # Build
-cat>>/mnt/home/$newUsername/jakeobsen-archlive/build.sh<<EOF
+cat>>/mnt/home/$newUsername/unlab-archlive/build.sh<<EOF
 #!/bin/bash
-rm -rf rm -rf /home/\$newUsername/jakeobsen-archlive/temp
-mkdir -p rm -rf /home/\$newUsername/jakeobsen-archlive/temp
-cd /home/\$newUsername/jakeobsen-archlive/
+rm -rf rm -rf /home/\$newUsername/unlab-archlive/temp
+mkdir -p rm -rf /home/\$newUsername/unlab-archlive/temp
+cd /home/\$newUsername/unlab-archlive/
 git pull
-sudo mkarchiso -v -w /home/\$newUsername/jakeobsen-archlive/temp /home/\$newUsername/jakeobsen-archlive/arch-archiso
+sudo mkarchiso -v -w /home/\$newUsername/unlab-archlive/temp /home/\$newUsername/unlab-archlive/arch-archiso
 EOF
-chmod +x /mnt/home/$newUsername/jakeobsen-archlive/build.sh
-arch-chroot /mnt /home/$newUsername/jakeobsen-archlive/build.sh
+chmod +x /mnt/home/$newUsername/unlab-archlive/build.sh
+arch-chroot /mnt /home/$newUsername/unlab-archlive/build.sh
 
 # Reset all perms
 arch-chroot /mnt chown $newUsername:$newUsername -R /home/$newUsername
